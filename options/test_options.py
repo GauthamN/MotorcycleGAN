5c9b25f444322aef9de0dacf2bd486e3c5dc28b8
Gautham Nekkanti <onegautham@gmail.com>
2018-12-25 11:02:41 +0000

    Add missing flag in test options

--- options/test_options.py
+++ options/test_options.py
@@ -11,7 +11,7 @@ class TestOptions(BaseOptions):
         #  Dropout and Batchnorm has different behavioir during training and test.
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
         parser.add_argument('--num_test', type=int, default=50, help='how many test images to run')
-
+        parser.add_argument('--lambda_identity', type=int, default=0.0, help='how many test images to run')
         parser.set_defaults(model='test')
         # To avoid cropping, the loadSize should be the same as fineSize
         parser.set_defaults(loadSize=parser.get_default('fineSize'))
